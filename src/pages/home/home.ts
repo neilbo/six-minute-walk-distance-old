73dd811c2cb9f87b8bbd24068a624992ee4c18a0
Neil Estandarte <neilbo21@gmail.com>
2017-04-01 14:35:36 +1000

    #4 Basic validation. Calc method only does Male calc.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,51 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { AgeValidator } from  '../../validators/age-validator';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  metricForm: FormGroup;
+  pageTitle: string = 'Home';
+  methodType: string = 'metric';
+  submitAttempt: boolean;
+  maleDistance: any;
+  
+  constructor(public navCtrl: NavController, 
+              public formBuilder: FormBuilder,
+              public alertCtrl: AlertController) {
+      this.metricForm = this.formBuilder.group({
+        cm: ['', Validators.required],
+        kg: ['', Validators.required],
+        ageMetric: ['', AgeValidator.isValid]
+      });
   }
 
-  pageTitle: string = 'Home';
-  methodType: string = "metric";
+  calculateMetric() {
+    this.submitAttempt = true;
+    //TODO: Add in function for Gender
+    // Female = (2.11 x heightcm) - (2.29 x weightkg) - (5.78 x age) + 667 m
+    let metric = this.metricForm.value;
+    let formEmpty = _.isEmpty(metric.cm) ||
+    _.isEmpty(metric.ageMetric) ||
+    _.isEmpty(metric.kg); 
 
+    if (!formEmpty) {
+      this.maleDistance = ((7.57 * metric.cm) - (5.02 * metric.ageMetric) - (1.76 * metric.kg) - 309).toFixed(2);
+      this.showDistance(this.maleDistance);
+    }
+  }
+
+  showDistance(distance) {
+    let alert = this.alertCtrl.create({
+      title: distance + 'm',
+      subTitle: '',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 }
